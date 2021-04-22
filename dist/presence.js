06496866ba5d2a102b3d860470259e2b4ee72a88
Bruno Gabriel Cal Olivera <br1calolivera@gmail.com>
2021-04-22 01:33:28 -0300

    Classes presence added

--- dist/presence.js
+++ dist/presence.js
@@ -1,11 +1,17 @@
 const presence = new Presence({
-    clientId: "834118889734012969"
+    clientId: "834628404233240628"
 }), tiempoEstimado = Math.floor(Date.now() / 1000);
+let available_logos;
+fetch("https://api.jsonbin.io/b/6080f3c956c62a0c0e8a28a3")
+    .then(res => res.json())
+    .then(data => available_logos = data.available_logos)
+    .then(() => console.log(available_logos));
 presence.on("UpdateData", async () => {
     const presenceData = {
         details: "Pagina desconocida",
-        largeImageKey: "lg"
+        largeImageKey: "lg-dark"
     };
+    console.log("act");
     if (document.location.pathname == "/home" || !document.location.pathname) {
         presenceData.state = "Inicio";
         presenceData.startTimestamp = tiempoEstimado;
@@ -30,6 +36,19 @@ presence.on("UpdateData", async () => {
         presenceData.state = "Viendo Platzi Live";
         presenceData.startTimestamp = tiempoEstimado;
         delete presenceData.details;
+    }
+    else if (document.location.pathname.includes("/clases/") && document.location.pathname.split("/").filter(Boolean).length === 2) {
+        const course = document.querySelector(".CourseDetail-left-title");
+        const teacher = document.querySelector(".TeacherList-full-name");
+        const pathNameSplitted = document.location.pathname.split("/").filter(Boolean);
+        presenceData.state = `de ${teacher.textContent}`;
+        presenceData.details = course.textContent;
+        if (available_logos.includes(pathNameSplitted[1])) {
+            presenceData.largeImageKey = pathNameSplitted[1].toString();
+        }
+        else {
+            presenceData.largeImageKey = "lg-dark";
+        }
     }
     presence.setActivity(presenceData);
 });
